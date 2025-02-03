73b809bdd3e59ba29900d375c2946bacc09051df
Anipreet-Chowdhury <anipreet.c@gmail.com>
2025-02-03 03:26:48 -0500

    Pushing for deployment

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,7 +6,7 @@ class NodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Node
         fields = ['id', 'label', 'description', 'impact', 'risk_level', 
-                 'current_status', 'date_of_completion', 'owner', 
+                 'current_status', 'date_of_completion', 'owner',  
                  'x_position', 'y_position']
 
 class EdgeSerializer(serializers.ModelSerializer):
